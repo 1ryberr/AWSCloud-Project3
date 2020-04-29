b6a9984a10f346bc211c8238f593dd500f21e8ae
ryanberry <ryber8@aol.com>
2020-04-29 08:03:38 -0400

    first commit

---
+++ udacity-restapi-feed/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramberrydev",
+    "password": "abc123me",
+    "database": "postgres",
+    "host": "udagramberrydev.cijwtqeoehmz.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_reigion": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-ruttner-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
---
+++ udacity-restapi-feed/src/sequelize.ts
@@ -0,0 +1,23 @@
+import {Sequelize} from 'sequelize-typescript';
+import { config } from './config/config';
+
+
+const c = config.dev;
+
+// Instantiate new Sequelize instance!
+export const sequelize = new Sequelize({
+  "username": c.username,
+  "password": c.password ,     
+  "database": c.database,
+  "host":  c.host,
+
+  dialect: 'postgres',
+   
+  storage: ':memory:', 
+});
+
+sequelize.authenticate().then(() => {
+  console.log("Success!");
+}).catch((err) => {
+  console.log(err);
+});
